f9148e36b87984f6a6aa51ed133a5c8f553f86a2
Cyrille Levandowski <cyrille.levandowski@zenika.com>
2017-09-11 16:17:06 +0200

    affichage du token uniqument

--- init-swarm-cluster.sh
+++ init-swarm-cluster.sh
@@ -2,8 +2,8 @@
 
 docker swarm init --advertise-addr $1
 
-token_manager=$(docker swarm join-token manager)
-token_worker=$(docker swarm join-token worker)
+token_manager=$(docker swarm join-token manager -q)
+token_worker=$(docker swarm join-token worker -q)
 
 echo $token_manager $token_worker
 
