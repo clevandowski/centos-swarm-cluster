a8da543b0ecf410598619a83ef2a36dde751acee
Cyrille Levandowski <cyrille.levandowski@zenika.com>
2017-09-11 16:03:36 +0200

    Remplacement des tirets par des underscores dans le nom des variables

--- init-swarm-cluster.sh
+++ init-swarm-cluster.sh
@@ -2,8 +2,8 @@
 
 docker swarm init --advertise-addr $1
 
-token-manager=$(docker swarm join-token manager)
-token-worker=$(docker swarm join-token worker)
+token_manager=$(docker swarm join-token manager)
+token_worker=$(docker swarm join-token worker)
 
-echo $token-manager $token-worker
+echo $token_manager $token_worker
 
